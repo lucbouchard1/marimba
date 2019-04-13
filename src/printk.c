bf29498881087f2d72fb133dd972ab57bf71bb20
Luc Bouchard <lbouchard010@gmail.com>
2019-04-13 15:15:40 -0700

    Improve printk implementation

--- src/printk.c
+++ src/printk.c
@@ -4,71 +4,74 @@
 #include "printk.h"
 #include "vga.h"
 
-#define HANDLE_FORMAT_SPECIFIER(arg, type) \
-do { \
-switch (type) \
-{ \
-   case 'u': \
-      print_uint64(arg); \
-      break; \
-   case 'd': \
-      print_int64(arg); \
-      break; \
-   case 'x': \
-      print_uint64_hex(arg); \
-      break; \
-   default: \
-      break; \
-} \
+#define HANDLE_FORMAT_SPECIFIER(arg, type, specifier) \
+do {                                  \
+switch (specifier)                    \
+{                                     \
+   case 'u':                          \
+      print_u##type(arg);             \
+      break;                          \
+   case 'd':                          \
+      print_##type(arg);              \
+      break;                          \
+   case 'x':                          \
+      print_u##type##_hex(arg);       \
+      break;                          \
+   default:                           \
+      break;                          \
+}                                     \
 } while (0)
 
-void print_uint64(uint64_t d)
-{
-   char buf[64], *cur = buf+64;
-   *cur = 0;
-   cur--;
+#define DISP_UNSIGNED_VAL_DEC(d)      \
+char buf[64], *cur = buf+64;          \
+*(cur--) = 0;                         \
+if (!d) {                             \
+   VGA_display_char('0');             \
+   return;                            \
+}                                     \
+for (; d; d /= 10, cur--)             \
+   *cur = (d % 10) + '0';             \
+VGA_display_str(cur + 1);
 
-   if (!d) {
-      VGA_display_char('0');
-      return;
-   }
+#define DISP_UNSIGNED_VAL_HEX(d)      \
+char buf[64], *cur = buf+64;          \
+int digit;                            \
+*(cur--) = 0;                         \
+if (!d) {                             \
+   VGA_display_char('0');             \
+   return;                            \
+}                                     \
+for (; d; d /= 16, cur--) {           \
+   digit = (d % 16);                  \
+   if (digit < 10)                    \
+      *cur = digit + '0';             \
+   else                               \
+      *cur = (digit - 10) + 'a';      \
+}                                     \
+VGA_display_str(cur + 1);
 
-   for (; d; d /= 10, cur--)
-      *cur = (d % 10) + '0';
-   VGA_display_str(cur + 1);
-}
+#define DISP_SIGNED_VAL_DEC(d, type)  \
+if (d < 0) {                          \
+   VGA_display_char('-');             \
+   d *= -1;                           \
+}                                     \
+print_u##type(d);
 
-void print_uint64_hex(uint64_t d)
-{
-   char buf[64], *cur = buf+64;
-   unsigned int digit;
-   *cur = 0;
-   cur--;
+void print_uint64_t(uint64_t d){ DISP_UNSIGNED_VAL_DEC(d) }
+void print_uint64_t_hex(uint64_t d) { DISP_UNSIGNED_VAL_HEX(d) }
+void print_int64_t(int64_t d) { DISP_SIGNED_VAL_DEC(d, int64_t) }
 
-   if (!d) {
-      VGA_display_char('0');
-      return;
-   }
+void print_ulong(unsigned long d){ DISP_UNSIGNED_VAL_DEC(d) }
+void print_ulong_hex(unsigned long d) { DISP_UNSIGNED_VAL_HEX(d) }
+void print_long(long d) { DISP_SIGNED_VAL_DEC(d, long) }
 
-   for (; d; d /= 16, cur--) {
-      digit = (d % 16);
-      if (digit < 10)
-         *cur = digit + '0';
-      else
-         *cur = (digit - 10) + 'a';
-   }
-   VGA_display_str(cur + 1);
-}
+void print_uint(unsigned int d){ DISP_UNSIGNED_VAL_DEC(d) }
+void print_uint_hex(unsigned int d) { DISP_UNSIGNED_VAL_HEX(d) }
+void print_int(int d) { DISP_SIGNED_VAL_DEC(d, int) }
 
-void print_int64(int64_t d)
-{
-   if (d < 0) {
-      VGA_display_char('-');
-      d *= -1;
-   }
-
-   print_uint64(d);
-}
+void print_ushort(unsigned short d){ DISP_UNSIGNED_VAL_DEC(d) }
+void print_ushort_hex(unsigned short d) { DISP_UNSIGNED_VAL_HEX(d) }
+void print_short(short d) { DISP_SIGNED_VAL_DEC(d, short) }
 
 int printk(const char *fmt, ...)
 {
@@ -96,24 +99,24 @@ int printk(const char *fmt, ...)
 
             case 'p':
                ptr_arg = va_arg(args, void *);
-               print_uint64_hex((uint64_t)ptr_arg);
+               print_uint64_t_hex((uint64_t)ptr_arg);
                break;
 
             case 'l':
                l_arg = va_arg(args, long);
-               HANDLE_FORMAT_SPECIFIER(l_arg, *(cur + 2));
+               HANDLE_FORMAT_SPECIFIER(l_arg, long, *(cur + 2));
                cur++;
                break;
 
             case 'h':
                i_arg = va_arg(args, int);
-               HANDLE_FORMAT_SPECIFIER(i_arg, *(cur + 2));
+               HANDLE_FORMAT_SPECIFIER(i_arg, short, *(cur + 2));
                cur++;
                break;
 
             case 'q':
                q_arg = va_arg(args, int64_t);
-               HANDLE_FORMAT_SPECIFIER(q_arg, *(cur + 2));
+               HANDLE_FORMAT_SPECIFIER(q_arg, int64_t, *(cur + 2));
                cur++;
                break;
 
@@ -121,7 +124,7 @@ int printk(const char *fmt, ...)
             case 'u':
             case 'x':
                i_arg = va_arg(args, int);
-               HANDLE_FORMAT_SPECIFIER(i_arg, *(cur + 1));            
+               HANDLE_FORMAT_SPECIFIER(i_arg, int, *(cur + 1));            
                break;
 
             case '%':
