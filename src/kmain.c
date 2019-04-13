7c4e831ba9f0096f4566ec70ae427ee7f0e4c013
Luc Bouchard <lbouchard010@gmail.com>
2019-04-13 12:15:54 -0700

    Finish printk

--- src/kmain.c
+++ src/kmain.c
@@ -29,10 +29,13 @@ void kmain(void)
    // }
    // VGA_display_str("Hello World\n");
    // VGA_display_str("!*&@&E()QWDJLASDOKJSKDHFWKL");
+   long x = -200;
 
    printk("Integer %d\n", -128);
-   printk("Char %c\n", 'H');
+   printk("Char %c %c %c %c\n", 'H', 'i', 'J', 'K');
    printk("String %s %d\n", "Hello World", 1000);
+   printk("Hex 0x%x\n", 0x12345abc);
+   printk("Shorts %ld %hd %hd", x, 13, 14);
 
    while(1);
 }
--- src/printk.c
+++ src/printk.c
@@ -10,8 +10,35 @@ void print_uint64(uint64_t d)
    *cur = 0;
    cur--;
 
+   if (!d) {
+      VGA_display_char('0');
+      return;
+   }
+
    for (; d; d /= 10, cur--)
       *cur = (d % 10) + '0';
+   VGA_display_str(cur + 1);
+}
+
+void print_uint64_hex(uint64_t d)
+{
+   char buf[64], *cur = buf+64;
+   unsigned int digit;
+   *cur = 0;
+   cur--;
+
+   if (!d) {
+      VGA_display_char('0');
+      return;
+   }
+
+   for (; d; d /= 16, cur--) {
+      digit = (d % 16);
+      if (digit < 10)
+         *cur = digit + '0';
+      else
+         *cur = (digit - 10) + 'a';
+   }
    VGA_display_str(cur + 1);
 }
 
@@ -30,6 +57,9 @@ int printk(const char *fmt, ...)
    int i_arg;
    unsigned int ui_arg;
    const char *cur, *str_arg;
+   void *ptr_arg;
+   long l_arg;
+   int64_t q_arg;
    va_list args;
    va_start(args, fmt);
 
@@ -53,10 +83,81 @@ int printk(const char *fmt, ...)
                break;
 
             case 'u':
-               ui_arg = va_arg(args, int);
+               ui_arg = va_arg(args, unsigned int);
                print_uint64(ui_arg);
                break;
-         
+
+            case 'x':
+               ui_arg = va_arg(args, unsigned int);
+               print_uint64_hex(ui_arg);
+               break;
+
+            case 'l':
+               l_arg = va_arg(args, long);
+               switch (*(cur + 2))
+               {
+                  case 'u':
+                     print_uint64(l_arg);
+                     break;
+                  case 'd':
+                     print_int64(l_arg);
+                     break;
+                  case 'x':
+                     print_uint64_hex(l_arg);
+                     break;
+                  default:
+                     break;
+               }
+               cur++;
+               break;
+
+            case 'h':
+               i_arg = va_arg(args, int);
+               switch (*(cur + 2))
+               {
+                  case 'u':
+                     print_uint64(i_arg);
+                     break;
+                  case 'd':
+                     print_int64(i_arg);
+                     break;
+                  case 'x':
+                     print_uint64_hex(i_arg);
+                     break;
+                  default:
+                     break;
+               }
+               cur++;
+               break;
+
+            case 'q':
+               q_arg = va_arg(args, int64_t);
+               switch (*(cur + 2))
+               {
+                  case 'u':
+                     print_uint64(q_arg);
+                     break;
+                  case 'd':
+                     print_int64(q_arg);
+                     break;
+                  case 'x':
+                     print_uint64_hex(q_arg);
+                     break;
+                  default:
+                     break;
+               }
+               cur++;
+               break;
+
+            case 'p':
+               ptr_arg = va_arg(args, void *);
+               print_uint64_hex((uint64_t)ptr_arg);
+               break;
+
+            case '%':
+               VGA_display_char('%');               
+               break;
+
             default:
                break;
          }
