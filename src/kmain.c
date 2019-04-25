1ccc9299e4e84091f4fcfc77480fff7fb3e5d0d8
Luc Bouchard <lbouchard010@gmail.com>
2019-04-25 10:07:04 -0700

    Halt CPU when waiting for interrupt

--- src/kmain.c
+++ src/kmain.c
@@ -25,41 +25,6 @@ void kmain(void)
    kdev = init_ps2(1);
    IRQ_set_handler(0x21, keyboard_isr, kdev);
    IRQ_clear_mask(0x21); // Enable interrupts from keyboard!!
-   while(1);
-   
-   // for (counter = 1; counter < 31; counter++) {
-   //    for (i = 0; i < counter; i++) {
-   //       VGA_display_str("a");
-   //       delay = 5000000;
-   //       while (delay--);
-   //    }
-   //    VGA_display_char('\n');
-   // }
-
-   // VGA_clear();
-   // VGA_display_str("\n\n\n\n\n\n\n");
-
-   // for (counter = 1; counter < 95; counter++) {
-   //    VGA_display_char('\r');
-   //    for (i = 0; i < counter; i++)
-   //       VGA_display_str("#");
-   //    delay = 40000000;
-   //    while (delay--);
-   // }
-   // VGA_display_str("Hello World\n");
-   // VGA_display_str("!*&@&E()QWDJLASDOKJSKDHFWKL");
-   long x = -200;
-   int p;
-
-   printk("Integer %d %%\n", -128);
-   printk("Char %c %c %c %c\n", 'H', 'i', 'J', 'K');
-   printk("String %s %x\n", "Hello World", -1000);
-   printk("Hex 0x%x\n", 0x12345abc);
-   printk("Hex 0x%X\n", 0x12345abc);
-   printk("Shorts %ld %hd %hd\n", x, 13, 14);
-   printk("Pointer %p", (void *)&p);
-
-   init_ps2();
-
-   while(1);
+   while(1)
+      asm("hlt;");
 }
