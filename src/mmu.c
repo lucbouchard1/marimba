342c705d744c9ddc58d7b7d331d2af0f043745ae
Luc Bouchard <lbouchard010@gmail.com>
2019-05-07 11:39:05 -0700

    Convert excluded sections to page boundry exclusions

--- src/mmu.c
+++ src/mmu.c
@@ -1,3 +1,4 @@
+#include <stdint.h>
 #include "types.h"
 #include "mmu.h"
 #include "printk.h"
@@ -30,6 +31,7 @@ int MMU_init(struct SystemMMap *map)
 {
    struct SegmentList excluded[MAX_EXCLUDE_SEGMENTS];
    struct SegmentList *ex_head, *prev, *curr;
+   uint8_t *new_base;
    int i;
 
    if (map->num_kernel_sects > MAX_EXCLUDE_SEGMENTS) {
@@ -39,25 +41,27 @@ int MMU_init(struct SystemMMap *map)
 
    /* Copy kernel segments into excluded segments array */
    ex_head = &excluded[0];
-   prev = NULL;
+   prev = 0;
    for (i = 0; i < map->num_kernel_sects; i++) {
       excluded[i].s.base = map->kernel_sects[i].base;
       excluded[i].s.len = map->kernel_sects[i].length;
-      excluded[i].next = NULL;
+      excluded[i].next = 0;
       if (prev)
          prev->next = &excluded[i];
       prev = &excluded[i];
    }
 
    /* Round excluded sections to page boundries */
-   ROUND down the base address, round up the length. Shouldnt cause any issues
-   for (curr = ex_head->next, prev = ex_head; curr; prev = curr, curr = curr->next) {
-      if (prev->s.base + prev->s.len < curr->s.base)
-         continue;
-
-      prev->s.len = max(prev->s.len, (curr->s.base + curr->s.len) - prev->s.base);
-      prev->next = curr->next;
-      curr = prev;
+   for (curr = ex_head; curr; curr = curr->next) {
+      /* Round down base to page offset */
+      new_base = (void *)((uint64_t)curr->s.base - ((uint64_t)curr->s.base % MMU_PAGE_SIZE));
+      curr->s.len += curr->s.base - new_base;
+      curr->s.base = new_base;
+      /* Round up length to page size */
+      if (curr->s.len % MMU_PAGE_SIZE) {
+         curr->s.len += MMU_PAGE_SIZE;
+         curr->s.len -= (curr->s.len % MMU_PAGE_SIZE);
+      }
    }
 
    /* Reduce overlapping kernel segments into contiguous segments */
