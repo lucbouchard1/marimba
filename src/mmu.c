b99e8a3de6b0a3b7b6642727959b49af1c11be98
Luc Bouchard <lbouchard010@gmail.com>
2019-05-19 14:26:48 -0700

    Further improve naming in MMU code

--- src/mmu.c
+++ src/mmu.c
@@ -4,6 +4,7 @@
 #include "printk.h"
 #include "string.h"
 #include "utils.h"
+#include "mmap.h"
 
 #define MAX_FREE_SEGMENTS 32
 #define MAX_EXCLUDE_SEGMENTS 128
@@ -190,7 +191,7 @@ int MMU_init(struct PhysicalMMap *map)
    ex_head = mmu_compute_excluded(map, excluded);
    mmu_compute_free_segments(&mmu_state, map, ex_head);
 
-   mmu_state.page_table = MMU_frame_alloc();
+   mmu_state.page_table = MMU_alloc_frame();
    if (!mmu_state.page_table)
       return -1;
 
@@ -201,7 +202,7 @@ int MMU_init(struct PhysicalMMap *map)
    return 0;
 }
 
-void *MMU_frame_alloc()
+void *MMU_alloc_frame()
 {
    void *ret;
 
@@ -222,7 +223,7 @@ void *MMU_frame_alloc()
    return ret;
 }
 
-void MMU_frame_free(void *pf)
+void MMU_free_frame(void *pf)
 {
    if (!mmu_state.free_page_head) {
       mmu_state.free_page_head = pf;
@@ -246,10 +247,10 @@ void MMU_stress_test()
    for (cycle = 0; cycle <= total_cycles; cycle++) {
       printk("Allocating and freeing 100 pages [%d/%d]", cycle, total_cycles);
       for (i = 0; i < 100; i++)
-         page_buff[i] = MMU_frame_alloc();
+         page_buff[i] = MMU_alloc_frame();
 
       for (i = 0; i < 100; i++)
-         MMU_frame_free(page_buff[i]);
+         MMU_free_frame(page_buff[i]);
       printk("\r");
    }
    printk("\nDone.\n");
@@ -257,32 +258,32 @@ void MMU_stress_test()
    printk("Bit Pattern Stress Test:\n");
    printk("   applying bit patterns on all pages...\n");
    for (cycle = 0; 1; cycle++) {
-      res = MMU_frame_alloc();
+      res = MMU_alloc_frame();
       /* Check if we've gotten through all the pages by looking for the bit pattern */
       if (!memcmp(&((char *)res)[bit_patt_off], bit_patt_base, bit_patt_len))
          break;
       /* Write bit pattern to page */
       memcpy(&((char *)res)[bit_patt_off], bit_patt_base, bit_patt_len);
       ((int *)res)[bit_patt_off + bit_patt_len] = cycle;
-      MMU_frame_free(res);
+      MMU_free_frame(res);
    }
    printk("   applied bit pattern to %d pages...\n", cycle);
    printk("   checking bit patterns on all pages...\n");
    for (i = 0; i < cycle; i++) {
-      MMU_frame_free(res);
+      MMU_free_frame(res);
       if (memcmp(&((char *)res)[bit_patt_off], bit_patt_base, bit_patt_len) ||
                ((int *)res)[bit_patt_off + bit_patt_len] != i)
          printk("Bit Pattern Failure!\n");
-      res = MMU_frame_alloc();
+      res = MMU_alloc_frame();
    }
-   MMU_frame_free(res);
+   MMU_free_frame(res);
    printk("   checked bit pattern on %d pages...\n", i);
    printk("Done.\n");
 
    printk("Max Allocation Stress Test:\n");
-   res = MMU_frame_alloc();
+   res = MMU_alloc_frame();
    for (cycle = 1; res; cycle++)
-      res = MMU_frame_alloc();
+      res = MMU_alloc_frame();
    printk("Page frame alloc returned NULL after allocating %d pages\n", cycle);
    printk("Done.\n");
 }
--- src/mmu.h
+++ src/mmu.h
@@ -4,8 +4,8 @@
 #include "paging.h"
 
 int MMU_init(struct PhysicalMMap *map);
-void *MMU_frame_alloc();
-void MMU_frame_free(void *pf);
+void *MMU_alloc_frame();
+void MMU_free_frame(void *pf);
 void MMU_stress_test();
 
 #endif
